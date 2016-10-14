5bbd906965066c0ca9f5091c640e7831a2cb5a78
Abhisheksrm236 <pratapraoabhishek43@gmail.com>
2016-10-14 12:12:50 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.js'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
